4b0b04f3d4250ec06b12f13785e66e65ec9107a6
Zike <rrozial535@gmail.com>
2025-09-10 11:37:35 +0300

    KS-PRACTICE-1-4

---
+++ src/main/kotlin/Lesson_1Practice/Lesson_1Practice4.kt
@@ -0,0 +1,11 @@
+package org.example.Lesson_1Practice
+
+fun main() {
+    val  length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Int = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Short = 32700
+
+}
