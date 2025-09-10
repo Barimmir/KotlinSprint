c7e1dd33ed874bc9df65630390cf6d1f4b32e37d
Zike <rrozial535@gmail.com>
2025-09-10 21:46:49 +0300

    KS-PRACTICE-2-4

---
+++ src/main/kotlin/Lesson_2Practice/Lesson_2Practice4.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson_2Practice
+
+fun main() {
+
+    val cristal = 7
+    val iron = 11
+    val bafCristal = (cristal * 20) / 100
+    val bafIron: Int = (iron * 20) / 100
+
+    print("Кристалы:")
+    print(bafCristal)
+    println()
+    print("Железо:")
+    print(bafIron)
+
+}
