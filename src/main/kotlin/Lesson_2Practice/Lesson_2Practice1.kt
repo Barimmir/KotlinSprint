1f9fa5fc90c1e02913ca2d738148e3ee0bde9ccd
Zike <rrozial535@gmail.com>
2025-09-10 15:30:51 +0300

    KS-PRACTICE-2-1

---
+++ src/main/kotlin/Lesson_2Practice/Lesson_2Practice1.kt
@@ -0,0 +1,11 @@
+package org.example.Lesson_2Practice
+
+fun main() {
+
+    val people = 4F
+    val sumBal= 3+4+3+5
+    val  averageScore: Float = sumBal / people
+
+    println(averageScore)
+
+}
