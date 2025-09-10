dfa43db39292648ce1e6c868999c384103f6b108
Zike <rrozial535@gmail.com>
2025-09-10 10:08:52 +0300

    KS-PRACTICE-1-2

---
+++ src/main/kotlin/Lesson_1Practice/Lesson_1Practice2.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson_1Practice
+
+fun main() {
+
+    val numberOfOrder: Int = 75
+    val gratitudeMassage : String = "Огромное спасибо за покупку!\n" +
+            "Надеемся, вы воспользуетесь нашими услугами снова!"
+    var numberOfEmployees: Int = 2000
+
+    println(numberOfOrder)
+    println(gratitudeMassage)
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+
+}
