743e6aa1a8ec5920beae80f5a3abf00ee86cb395
Zike <rrozial535@gmail.com>
2025-09-11 23:24:14 +0300

    KS-PRACTICE-3-1- create greetings

---
+++ src/main/kotlin/Lesson_3Practice/Lesson_3Practice1.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson_3Practice
+
+fun main() {
+
+    val userName ="Сенпай"
+    var greetings ="Охаё"
+
+    println("$greetings,$userName!")
+
+    greetings ="Ара-ара, я тебя ждала"
+
+    println("$greetings $userName")
+}
