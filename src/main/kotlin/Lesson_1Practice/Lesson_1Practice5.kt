915eb954592895af88c8072178919f2619a6082d
Zike <rrozial535@gmail.com>
2025-09-12 20:00:17 +0300

    KS-PRACTICE-1-5-Гагарин в космосе

--- src/main/kotlin/Lesson_1Practice/Lesson_1Practice5.kt
+++ src/main/kotlin/Lesson_1Practice/Lesson_1Practice5.kt
@@ -2,13 +2,14 @@ package org.example.Lesson_1Practice
 
 fun main() {
     val seconds = 6480
-    val minute: Int = seconds / secAndMin
-    val hour : Int = minute / minAndHour
-    val remainderMinute = minute % minAndHour
-    val remainderSecond = seconds % secAndMin
+    val minute: Int = seconds / SEC_AND_MIN
+    val hour : Int = minute / MIN_AND_HOUR
+    val remainderMinute = minute % MIN_AND_HOUR
+    val remainderSecond = seconds % SEC_AND_MIN
     val massage = String.format("O$hour:$remainderMinute:0$remainderSecond")
 
     println(massage)
 }
-const val secAndMin = 60
-const val minAndHour = 60
+const val SEC_AND_MIN = 60
+const val MIN_AND_HOUR = 60
+
