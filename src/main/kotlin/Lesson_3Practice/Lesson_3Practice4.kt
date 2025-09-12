4a0431adda4060e291015a84329a0f792b6d779f
Zike <rrozial535@gmail.com>
2025-09-12 14:15:01 +0300

    KS-PRACTICE-3-4-Шахматы и логи ходов

---
+++ src/main/kotlin/Lesson_3Practice/Lesson_3Practice4.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson_3Practice
+
+fun main() {
+    var start = "E2"
+    var end = "E4"
+    var numberMove = 1
+    var message ="$start,$end,$numberMove"
+
+    println(message)
+
+    start = "D2"
+    end = "D3"
+    numberMove = 2
+    message ="$start,$end,$numberMove"
+
+    println(message)
+}
