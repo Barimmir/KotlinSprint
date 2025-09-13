78466a85913a6389c9cd82d8b5b888645641ac57
Zike <rrozial535@gmail.com>
2025-09-14 00:04:28 +0300

    KS-3-4-Поправил вывод

--- src/main/kotlin/Lesson_3Practice/Lesson_3Practice4.kt
+++ src/main/kotlin/Lesson_3Practice/Lesson_3Practice4.kt
@@ -5,11 +5,11 @@ fun main() {
     var end = "E4"
     var numberMove = 1
 
-    println("$start,$end,$numberMove")
+    println("$start-$end;$numberMove")
 
     start = "D2"
     end = "D3"
     numberMove = 2
 
-    println("$start,$end,$numberMove")
+    println("$start-$end;$numberMove")
 }
