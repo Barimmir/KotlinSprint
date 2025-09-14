09e0f206335c3cc3c7d2c80a9df3117a174291e3
Zike <rrozial535@gmail.com>
2025-09-14 20:38:15 +0300

    KS-4-2-Таможенный терминал начало

---
+++ src/main/kotlin/Lesson4Practice/Lesson_4Practice2.kt
@@ -0,0 +1,29 @@
+package org.example.Lesson4Practice
+
+fun main() {
+
+    val baggageKilograms1 = 20
+    val baggageLiters1 = 80
+    val baggageKilograms2 = 50
+    val baggageLiters2 = 100
+    val chek: Boolean = (MIN_KILOGRAMS < baggageKilograms1) && (baggageKilograms1 < MIN_KILOGRAMS)
+            && (MIN_LITERS < baggageLiters1) && (baggageLiters1 < MAX_LITERS)
+    val chek2: Boolean = (MIN_KILOGRAMS < baggageKilograms2) && (baggageKilograms2 < MAX_KILOGRAMS)
+            && (MIN_LITERS < baggageLiters2) && (baggageLiters2 < MAX_LITERS)
+
+    println(
+        "Груз с весом $baggageKilograms1 кг и объемом $baggageLiters1 л " +
+                "соответствует категории 'Average':$chek"
+    )
+    println(
+        "Груз с весом $baggageKilograms2 кг и объемом $baggageLiters2 л " +
+                "соответствует категории 'Average':$chek2"
+    )
+
+}
+
+const val MIN_KILOGRAMS = 36
+const val MAX_KILOGRAMS = 100
+const val MIN_LITERS = 0
+const val MAX_LITERS = 99
+
