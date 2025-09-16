e2291c3c99fd72a3a791dfa08504a63ede78f47d
Zike <rrozial535@gmail.com>
2025-09-16 09:52:13 +0300

    KS-4-5-Добавил константы

--- src/main/kotlin/Lesson4Practice/Lesson_4Practice5.kt
+++ src/main/kotlin/Lesson4Practice/Lesson_4Practice5.kt
@@ -11,10 +11,17 @@ fun main() {
     println("Погода благоприятная?(true/false)")
     val weather = readln().toBoolean()
 
-    val result = ((!broken && crew >= 55 && crew <= 70 && provisions > 50 && (weather || !weather))
-            || (broken && crew == 70 && provisions >= 50 && weather))
+    val result = ((broken == NO_DAMAGE && crew >= MIN_CREW && crew <= MAX_CREW
+            && provisions > MIN_PROVISIONS && (weather == GOOD_WEATHER || weather == !GOOD_WEATHER))
+            || (broken == !NO_DAMAGE && crew == MAX_CREW && provisions >= MIN_PROVISIONS
+            && weather == GOOD_WEATHER))
 
     println("Корабль может лететь: $result")
-
 }
+
+const val NO_DAMAGE = true
+const val MIN_CREW = 55
+const val MAX_CREW = 70
+const val MIN_PROVISIONS = 50
+const val GOOD_WEATHER = true
 
