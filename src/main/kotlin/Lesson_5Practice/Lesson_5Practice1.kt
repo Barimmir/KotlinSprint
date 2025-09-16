f9421b1286b174a9108f2ccecde52f9188034d4b
Zike <rrozial535@gmail.com>
2025-09-16 09:56:48 +0300

    KS-5-1-Добавил переменные со счётом

--- src/main/kotlin/Lesson_5Practice/Lesson_5Practice1.kt
+++ src/main/kotlin/Lesson_5Practice/Lesson_5Practice1.kt
@@ -2,14 +2,17 @@ package org.example.Lesson_5Practice
 
 fun main() {
 
+    val a = 2
+    val b = 3
+    val c = a + b
     println(
         "Пожалуйста, решите пример:\n" +
-                "2 + 2 ="
+                "$a + $b ="
     )
     val answer = readln().toInt()
-    if (answer == 4) {
+    if (answer == c) {
         println("Добро пожаловать мешок с костями!")
-    } else{
+    } else {
         println("Иди своей дорогой сталкер,тебе здесь не рады!")
     }
 }
