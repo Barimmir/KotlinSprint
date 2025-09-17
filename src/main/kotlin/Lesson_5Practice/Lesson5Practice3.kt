6c14c2ad2ad5e004f30149bafad49c850f31e129
Zike <rrozial535@gmail.com>
2025-09-17 16:51:40 +0300

    KS-5-3-Сменил названия переменных

--- src/main/kotlin/Lesson_5Practice/Lesson5Practice3.kt
+++ src/main/kotlin/Lesson_5Practice/Lesson5Practice3.kt
@@ -2,8 +2,8 @@ package org.example.Lesson_5Practice
 
 fun main() {
 
-    val numberWin1 = 5
-    val numberWin2 = 17
+    val numberWin1 = 17
+    val numberWin2 = 5
 
     println(
         "Угадайте два числа от $MIN_NUMBER до $MAX_NUMBER для победы!\n" +
@@ -14,16 +14,16 @@ fun main() {
     println("Введите второе число:")
     val number2 = readln().toInt()
 
-    if (number1 == numberWin2 && number2 == numberWin1) {
+    if (number1 == numberWin1 && number2 == numberWin2) {
         println("Поздравляем! Вы выиграли главный приз!")
-    } else if ((number1 == numberWin2 && number2 != numberWin1) ||
-        (number1 != numberWin2 && number2 == numberWin1)
+    } else if ((number1 == numberWin1 && number2 != numberWin2) ||
+        (number1 != numberWin1 && number2 == numberWin2)
     ) {
         println("Вы выиграли утешительный приз!")
-        println("Правильные числа были $numberWin2 и $numberWin1")
+        println("Правильные числа были $numberWin1 и $numberWin2")
     } else {
         println("Неудача!")
-        println("Правильные числа были $numberWin2 и $numberWin1")
+        println("Правильные числа были $numberWin1 и $numberWin2")
     }
 }
 
