42f3526ac30e16a9646ceb64ca914dd5c0fe1255
Zike <rrozial535@gmail.com>
2025-09-17 10:28:15 +0300

    KS-4-3-Добавил переменные

--- src/main/kotlin/Lesson4Practice/Lesson_4Practice3.kt
+++ src/main/kotlin/Lesson4Practice/Lesson_4Practice3.kt
@@ -2,7 +2,13 @@ package org.example.Lesson4Practice
 
 fun main() {
 
-    val result: Boolean = WEATHER && AWNING && HUMIDITY == 20 && TIME_OF_YEAR != "Зима"
+    val weather = true
+    val awning = true
+    val humidity = 20
+    val timeOfYear = "Зима"
+
+    val result: Boolean = (weather == WEATHER) && (awning == AWNING) && (humidity == HUMIDITY)
+            && (timeOfYear == TIME_OF_YEAR)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $result")
 }
@@ -10,4 +16,4 @@ fun main() {
 const val WEATHER: Boolean = true
 const val AWNING: Boolean = true
 const val HUMIDITY: Int = 20
-const val TIME_OF_YEAR: String = "Зима"
+const val TIME_OF_YEAR: String = "Не зима"
