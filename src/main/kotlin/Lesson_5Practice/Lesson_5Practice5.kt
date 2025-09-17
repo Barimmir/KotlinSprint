f0f2a99fd6680b26934c76e983cc3f10a5596279
Zike <rrozial535@gmail.com>
2025-09-17 12:23:05 +0300

    KS-5-5-Лотерея всё сложней

---
+++ src/main/kotlin/Lesson_5Practice/Lesson_5Practice5.kt
@@ -0,0 +1,50 @@
+package org.example.Lesson_5Practice
+
+import kotlin.random.Random
+
+fun main() {
+
+    val randomNumbers = List(3) { Random.nextInt(0, 43) }
+
+    println(
+        "Добро пожаловтаь к богу рандома!\n" +
+                "Попробуй угадать три числа от 0 до 42!\n" +
+                "И помни, удача уважает удачливых!"
+    )
+
+    val userNumbers = mutableListOf<Int>()
+    for (num in 1..3) {
+        println("Введите число $num:")
+        val inputUser = readlnOrNull()?.toIntOrNull()
+
+
+        if (inputUser == null || inputUser !in 0..42) {
+            println(
+                "Даже для удачливого это слишком!\n" +
+                        "Вводи корректные числа!"
+            )
+            return
+        }
+        userNumbers.add(inputUser)
+    }
+    val scan = randomNumbers.intersect(userNumbers.toSet())
+    val result = scan.size
+
+    when (result) {
+        3 -> println("Джекпот! Ты самый удачливый на Земле!")
+        2 -> println("Неплохо! Два числа угаданы, а ты выйграл курпный приз!")
+        1 -> println(
+            "Лучше чем ничего! Ты угадал одно число и утешительный приз твой!\n" +
+                    "Тебя ведь мама всегда так называла?"
+        )
+
+        0 -> println(
+            "Удача любит удачливых, но видимо тебя никто не любит!\n" +
+                    "Ноль угаданных чисел!"
+        )
+    }
+    println("\nВыйгрышные числа: ${randomNumbers.sorted().joinToString(", ")}")
+    println("Твои числа: ${userNumbers.sorted().joinToString(", ")}")
+    println("Совпадения: ${scan.sorted().joinToString(", ")}")
+}
+
