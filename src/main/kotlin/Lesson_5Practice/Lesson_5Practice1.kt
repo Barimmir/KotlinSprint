638c598ce23408e5e1a98d6bbfbf3a52efd1f853
Zike <rrozial535@gmail.com>
2025-09-17 10:33:39 +0300

    KS-5-1-Изменил названия переменных

--- src/main/kotlin/Lesson_5Practice/Lesson_5Practice1.kt
+++ src/main/kotlin/Lesson_5Practice/Lesson_5Practice1.kt
@@ -2,15 +2,15 @@ package org.example.Lesson_5Practice
 
 fun main() {
 
-    val a = 2
-    val b = 3
-    val c = a + b
+    val num1 = 2
+    val num2 = 3
+    val sum = num1 + num2
     println(
         "Пожалуйста, решите пример:\n" +
-                "$a + $b ="
+                "$num1 + $num2 ="
     )
     val answer = readln().toInt()
-    if (answer == c) {
+    if (answer == sum) {
         println("Добро пожаловать мешок с костями!")
     } else {
         println("Иди своей дорогой сталкер,тебе здесь не рады!")
