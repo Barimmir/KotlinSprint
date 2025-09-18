46cbb01a1c4d11b5f34d765fad90e5f19b9b7351
Zike <rrozial535@gmail.com>
2025-09-18 16:40:55 +0300

    KS-5-3-Сделал неважным последовательность ввода

--- src/main/kotlin/Lesson_5Practice/Lesson5Practice3.kt
+++ src/main/kotlin/Lesson_5Practice/Lesson5Practice3.kt
@@ -14,10 +14,14 @@ fun main() {
     println("Введите второе число:")
     val number2 = readln().toInt()
 
-    if (number1 == numberWin1 && number2 == numberWin2) {
+    if ((number1 == numberWin1) && (number2 == numberWin2)
+        || (number1 == numberWin2) && (number2 == numberWin1)
+    ) {
         println("Поздравляем! Вы выиграли главный приз!")
     } else if ((number1 == numberWin1 && number2 != numberWin2) ||
-        (number1 != numberWin1 && number2 == numberWin2)
+        (number1 != numberWin1 && number2 == numberWin2) ||
+        (number1 == numberWin2) && (number2 != numberWin1) ||
+        (number1 != numberWin2) && (number2 == numberWin1)
     ) {
         println("Вы выиграли утешительный приз!")
         println("Правильные числа были $numberWin1 и $numberWin2")
