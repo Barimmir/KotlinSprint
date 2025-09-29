a2c50b606a2221ddf34210ddb9805b6bd53fbdb7
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-09-29 20:41:49 +0300

    KS-7-3-Поправил название переменной

--- src/main/kotlin/Lesson_7Practice/Lesson7Practice3.kt
+++ src/main/kotlin/Lesson_7Practice/Lesson7Practice3.kt
@@ -1,13 +1,12 @@
 package org.example.Lesson_7Practice
 
 fun main() {
-
     println(
         "Здравствуйте, введите чётное число до которого\n" +
                 "надо простроить прогрессию: "
     )
     val input = readln().toInt()
-    for (range in 0..input step 2) {
-        println(range)
+    for (number in 0..input step 2) {
+        println(number)
     }
 }
