5806cc2d56576d3bb24a33097318ca7ea35f99f1
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-09-29 20:07:10 +0300

    KS-7-2-добавил новый цикл

--- src/main/kotlin/Lesson_7Practice/Lesson7Practice2.kt
+++ src/main/kotlin/Lesson_7Practice/Lesson7Practice2.kt
@@ -2,22 +2,13 @@ package org.example.Lesson_7Practice
 
 fun main() {
 
-    var counter = 3
-
-    while (counter > 0) {
-        val range = (1000..9999).random()
+    do {
+        val authCode = (1000..9999).random()
         println(
-            "Ваш код авторизации: $range\n\n" +
+            "Ваш код авторизации: $authCode\n\n" +
                     "Введите код:"
         )
         val code = readln().toInt()
-        if (code == range) {
-            println("Код введён верно, добро пожаловать!")
-            return
-        } else {
-            counter--
-            println("Неверно!")
-            continue
-        }
-    }
+    } while (authCode != code)
+    println("Код введён верно, добро пожаловать!")
 }
