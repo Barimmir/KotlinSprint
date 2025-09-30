ec621ee3cc81c194eeafe46c04f5b7d6004acf64
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-09-30 15:16:12 +0300

    KS-7-5-Сделал обязательными наличе символов

--- src/main/kotlin/Lesson_7Practice/Lesson7Practice5.kt
+++ src/main/kotlin/Lesson_7Practice/Lesson7Practice5.kt
@@ -1,19 +1,30 @@
 package org.example.Lesson_7Practice
 
 fun main() {
-    val range1 = 'A'..'Z'
-    val range2 = 'a'..'z'
-    val range3 = '1'..'9'
-
-    val listRange = mutableListOf<Char>()
-    listRange.addAll(range1)
-    listRange.addAll(range2)
-    listRange.addAll(range3)
+    val capitaLettersRange = ('A'..'Z').toList()
+    val smallLettersRange = ('a'..'z').toList()
+    val numberRange = ('0'..'9').toList()
+    val allRange = capitaLettersRange + smallLettersRange + numberRange
 
     println("Задайте длинну пароля:")
     val input = readln().toInt()
+    if (input >= MIN_SYMBOL_IN_PASSWORD) {
+        val password = mutableListOf<Char>().apply {
+            add(capitaLettersRange.random())
+            add(smallLettersRange.random())
+            add(numberRange.random())
 
-    val password = List(input) { listRange.random() }.joinToString("")
+            repeat(input - DECREASE) {
+                add(allRange.random())
+            }
+            shuffled()
+        }.joinToString("")
+        println(password)
+    } else {
+        println("Недостаточная длина пароля, надо минимум $MIN_SYMBOL_IN_PASSWORD")
+    }
 
-    println(password)
 }
+
+const val MIN_SYMBOL_IN_PASSWORD = 6
+const val DECREASE = 3
