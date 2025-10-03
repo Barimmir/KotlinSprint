edbaa2f536e5de02a2668df17d89f2a4fa3667ca
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-10-03 11:17:38 +0300

    KS-9-2-Подправил

--- src/main/kotlin/Lesson_9Practice/Lesson9Practice2.kt
+++ src/main/kotlin/Lesson_9Practice/Lesson9Practice2.kt
@@ -8,12 +8,10 @@ fun main() {
                 "Желаете добавить свой?"
     )
     val userChoice = readln()
-    if (userChoice.equals("Да", true) && userChoice.equals("да", true)) {
+    if (userChoice.equals("да", true)) {
         println("Какой ингредиент вы хотите добавить?")
         val userInputIngredients = readln()
         listIngredients.add(userInputIngredients)
         println("Теперь в рецепте есть следующие ингредиенты: ${listIngredients.joinToString()}")
-    } else {
-        return
     }
 }
