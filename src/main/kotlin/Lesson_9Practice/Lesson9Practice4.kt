6a9e70966e537efc976bab6e4c9a8435b0c846c2
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-10-03 23:51:45 +0300

    KS-9-4-Обьеденил переменные

--- src/main/kotlin/Lesson_9Practice/Lesson9Practice4.kt
+++ src/main/kotlin/Lesson_9Practice/Lesson9Practice4.kt
@@ -2,13 +2,12 @@ package org.example.Lesson_9Practice
 
 fun main() {
     println("Здравствуйте, введите $NUMBER_OF_DISHES блюд через запятую с пробелом (, ):")
-    val userInputDishes = readln()
-    val fiveListOfIngredients = userInputDishes.split(", ").toMutableList()
-    if (fiveListOfIngredients.size != NUMBER_OF_DISHES) {
+    val ingredients = readln().split(", ")
+    if (ingredients.size != NUMBER_OF_DISHES) {
         println("Ошибка, введите пожалуйста $NUMBER_OF_DISHES блюд!")
         return
     }
-    val newFiveListOfIngredientsSorted = fiveListOfIngredients.sorted()
+    val newFiveListOfIngredientsSorted = ingredients.sorted()
     println(newFiveListOfIngredientsSorted.joinToString())
 }
 
