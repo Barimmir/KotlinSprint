494e2a4af34d4e031aad3761da342239aff2dc00
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-10-08 13:59:24 +0300

    KS-11-1-Подправил названия переменных

--- src/main/kotlin/Lesson_11Practice/Lesson11Practice1.kt
+++ src/main/kotlin/Lesson_11Practice/Lesson11Practice1.kt
@@ -1,32 +1,32 @@
 package org.example.Lesson_11Practice
 
-class User (
-    val userLogin: String,
-    val userPassword: String,
-    val userEmail: String,
-    val identifier: Int,
-) {}
+class User(
+    val id: Int,
+    val Login: String,
+    val Password: String,
+    val Email: String,
+)
 
 fun main() {
     val user1 = User(
-        userLogin = "Мамин бродяга",
-        userPassword = "нагибатор2016",
-        userEmail = "СамРешу@yandex.ru",
-        identifier = 228
+        id = 228,
+        Login = "Мамин бродяга",
+        Password = "нагибатор2016",
+        Email = "СамРешу@yandex.ru",
     )
     val user2 = User(
-        userLogin = "Папин симпотяга",
-        userPassword = "ДедИнсайд1998",
-        userEmail = "АнимэСосед@mail.ru",
-        identifier = 322
+        id = 322,
+        Login = "Папин симпотяга",
+        Password = "ДедИнсайд1998",
+        Email = "АнимэСосед@mail.ru",
     )
-    println(user1.userLogin)
-    println(user1.userPassword)
-    println(user1.userEmail)
-    println(user1.identifier)
+    println(user1.id)
+    println(user1.Login)
+    println(user1.Password)
+    println(user1.Email)
     println()
-    println(user2.userLogin)
-    println(user2.userPassword)
-    println(user2.userEmail)
-    println(user2.identifier)
+    println(user2.id)
+    println(user2.Login)
+    println(user2.Password)
+    println(user2.Email)
 }
