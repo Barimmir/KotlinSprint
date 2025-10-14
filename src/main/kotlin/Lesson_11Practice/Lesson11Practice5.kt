7fbf4da16bc72b5d2430d06990873cb478b73eb5
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-10-14 17:40:33 +0300

    KS-11-5-Вынес классы и поправил переменную

--- src/main/kotlin/Lesson_11Practice/Lesson11Practice5.kt
+++ src/main/kotlin/Lesson_11Practice/Lesson11Practice5.kt
@@ -1,16 +1,16 @@
 package org.example.Lesson_11Practice
 
+class ForumMember(
+    val userId: Int,
+    val userName: String,
+)
+
+class ForumMessage(
+    val authorId: Int,
+    val message: String,
+)
+
 class Forum {
-    class ForumMember(
-        val userId: Int,
-        val userName: String,
-    )
-
-    class ForumMessage(
-        val authorId: Int,
-        val message: String,
-    )
-
     private val members = mutableListOf<ForumMember>()
     private val messages = mutableListOf<ForumMessage>()
     private var nextUserId = 1
@@ -47,7 +47,10 @@ fun main() {
 
     forum.createNewMessage(user1.userId, "Здарова бандиты! Как ваши дела?")
     forum.createNewMessage(user2.userId, "Ну привет мамин бандит, без тебя тут было уютнее!")
-    forum.createNewMessage(user1.userId, "Вчера бургер по рецепту из приложухи сделал и в туалете сижу целый день уже...")
+    forum.createNewMessage(
+        user1.userId,
+        "Вчера бургер по рецепту из приложухи сделал и в туалете сижу целый день уже..."
+    )
     forum.createNewMessage(user2.userId, "Азазаз, а ты больше верь приложениями! За нами следят! АВЭ ЛИНУУУУУУКС!")
 
     forum.printThread()
