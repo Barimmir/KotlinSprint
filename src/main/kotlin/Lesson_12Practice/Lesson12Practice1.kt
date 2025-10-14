3aeb154d31c5a94f29f575e57b521601853caaa3
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-10-14 12:13:19 +0300

    KS-12-1-Поправил переменные

--- src/main/kotlin/Lesson_12Practice/Lesson12Practice1.kt
+++ src/main/kotlin/Lesson_12Practice/Lesson12Practice1.kt
@@ -1,28 +1,28 @@
 package org.example.Lesson_12Practice
 
-class WeatherDay() {
-    var tempDay = 10
-    var tempNight = 0
-    var precipitation = false
+class WeatherDay{
+    var dayTemperature = 10
+    var nightTemperature = 0
+    var isPrecipitation = false
 
     fun printInfo() {
         println(
-            "Температура днём: $tempDay\n" +
-                    "Температура ночью: $tempNight\n" +
-                    "Осадки: $precipitation"
+            "Температура днём: $dayTemperature\n" +
+                    "Температура ночью: $nightTemperature\n" +
+                    "Осадки: $isPrecipitation"
         )
     }
 }
 
 fun main() {
     val day1 = WeatherDay()
-    day1.tempDay = 5
-    day1.tempNight = -5
-    day1.precipitation = true
+    day1.dayTemperature = 5
+    day1.nightTemperature = -5
+    day1.isPrecipitation = true
     val day2 = WeatherDay()
-    day2.tempDay = 15
-    day2.tempNight = 3
-    day2.precipitation = false
+    day2.dayTemperature = 15
+    day2.nightTemperature = 3
+    day2.isPrecipitation = false
 
     day1.printInfo()
     day2.printInfo()
