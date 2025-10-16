6b88d54ec138ff1182463ea9ebec2b6eea19721d
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-10-16 09:30:38 +0300

    KS-13-2-Поправил

--- src/main/kotlin/Lesson_13Practice/Lesson13Practice2.kt
+++ src/main/kotlin/Lesson_13Practice/Lesson13Practice2.kt
@@ -1,12 +1,12 @@
 package org.example.Lesson_13Practice
 
 class User2(
-   val name: String,
-   val number: Long,
-   val company: String? = null,
+    val name: String,
+    val number: Long,
+    val company: String? = null,
 ) {
     fun printInfo() {
-        println("Имя: $name, Номер: $number, Компания: $company")
+        println("Имя: $name\n Номер: $number\n Компания: ${company ?: "не указано"}")
     }
 }
 
@@ -14,7 +14,6 @@ fun main() {
     val user1 = User2(
         name = "Фрося",
         number = 89123456789,
-        company = null?: "Не указано"
     )
     user1.printInfo()
 }
