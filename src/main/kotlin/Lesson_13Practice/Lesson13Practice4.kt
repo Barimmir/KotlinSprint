9618834cc480cb28f46c753a501bae417460b2a5
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-10-17 17:24:52 +0300

    KS-13-4-Сделал бесконечный цикл с возможностью выхода

--- src/main/kotlin/Lesson_13Practice/Lesson13Practice4.kt
+++ src/main/kotlin/Lesson_13Practice/Lesson13Practice4.kt
@@ -16,22 +16,32 @@ class User4(
 
 fun main() {
     val phoneBook = mutableListOf<User4>()
+    while (true) {
+        println(
+            "Введите своё имя: \n" +
+                    "Внимание! Оставте поле пустым если хотите выйти из программы!"
+        )
+        val nameInput = readln().trim()
+        if (nameInput.isEmpty()) {
+            println("Выход из программы!")
+            break
+        }
+        println("Введите номер телефона:")
+        val numberInput = readln().toLongOrNull()
+        if (numberInput == null) {
+            println("Ошибка: номер телефона должен содержать только цифры! Запись не добавлена.")
+            return
+        }
+        println("Введите название компании:")
+        val companyInput = readln().trim()
+        val company = if (companyInput.isEmpty()) null else companyInput
 
-    println("Введите номер телефона:")
-    val numberInput = readln().toLongOrNull()
-    if (numberInput == null) {
-        println("Ошибка: номер телефона должен содержать только цифры! Запись не добавлена.")
-        return
+        val contact = User4(
+            name = null,
+            number = numberInput,
+            company = company
+        )
+        phoneBook.add(contact)
     }
-    println("Введите название компании:")
-    val companyInput = readln().trim()
-    val company = if (companyInput.isEmpty()) null else companyInput
-
-    val contact = User4(
-        name = null,
-        number = numberInput,
-        company = company
-    )
-    phoneBook.add(contact)
     phoneBook.forEach { it.printInfo() }
 }
