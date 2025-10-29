556847f77789841c64297895fb057989396ef7d7
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-10-29 15:20:59 +0300

    KS-16-1-Приватный кубик

---
+++ src/main/kotlin/Lesson_16Practice/Lesson16Practice1.kt
@@ -0,0 +1,12 @@
+package org.example.Lesson_16Practice
+
+class Cube {
+    private val number = (1 .. 6).random()
+
+    fun getNumber() = number
+}
+
+fun main() {
+    val cube = Cube()
+    println(cube.getNumber())
+}
