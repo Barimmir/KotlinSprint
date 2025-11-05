7f21a61a8dc6e6f59e0404e754ea027059122031
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-11-05 16:42:47 +0300

    KS-18-1-Поправил

--- src/main/kotlin/Lesson_18Practice/Lesson18Practice1.kt
+++ src/main/kotlin/Lesson_18Practice/Lesson18Practice1.kt
@@ -1,19 +1,19 @@
 package org.example.Lesson_18Practice
 
 class Order(
-    numberOfOrder: Int,
+    val numberOfOrder: Int,
 ) {
     fun printInfo(product: String) {
         println("Заказан товар: $product")
     }
 
     fun printInfo(product: List<String>) {
-        println("Заказан товар: $product")
+        println("Заказан товар: ${product.joinToString()}")
     }
 }
 
 fun main() {
     val order = Order(3)
     order.printInfo("Кружка")
-    order.printInfo(listOf("Круг надувной", "варежки", "шорты").joinToString())
+    order.printInfo(listOf("Круг надувной", "варежки", "шорты"))
 }
