5e6e1922c00e005cf6ab731e9f4cb5b405adcaf6
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-11-08 15:59:42 +0300

    KS-18-3-Поправил

--- src/main/kotlin/Lesson_18Practice/Lesson18Practice3.kt
+++ src/main/kotlin/Lesson_18Practice/Lesson18Practice3.kt
@@ -1,43 +1,65 @@
 package org.example.Lesson_18Practice
 
-open class Animal() {
-    open fun eat() = ""
+abstract class Animal(
+    open val animal: String,
+    open val eat: String
+) {
+    open fun eat(): String {
+        return "$animal ест -> $eat"
+    }
+
+    open fun sleep(): String {
+        return "$animal спит"
+    }
 }
 
 class Fox(
-    private val animal: String,
-    private val eat: String,
-) : Animal() {
+    override val animal: String = "Лиса",
+    override val eat: String = "ягоды",
+) : Animal(animal, eat) {
     override fun eat(): String {
-        return "$animal ест -> $eat"
+        return super.eat()
+    }
+
+    override fun sleep(): String {
+        return super.sleep()
     }
 }
 
 class Dog(
-    private val animal: String,
-    private val eat: String,
-) : Animal() {
+    override val animal: String = "Собака",
+    override val eat: String = "кости",
+) : Animal(animal, eat) {
     override fun eat(): String {
-        return "$animal ест -> $eat"
+        return super.eat()
+    }
+
+    override fun sleep(): String {
+        return super.sleep()
     }
 }
 
 class Cat(
-    private val animal: String,
-    private val eat: String,
-) : Animal() {
+    override val animal: String = "Кошка",
+    override val eat: String = "рыбу",
+) : Animal(animal, eat) {
     override fun eat(): String {
-        return "$animal ест -> $eat"
+        return super.eat()
+    }
+
+    override fun sleep(): String {
+        return super.sleep()
     }
 }
 
 fun main() {
     val listAnimal = mutableListOf<Animal>()
-    val dog = Dog("Собака", "кости")
-    val fox = Fox("Лиса", "ягоды")
-    val cat = Cat("Кошка", "рыбу")
+    val dog = Dog()
+    val fox = Fox()
+    val cat = Cat()
     listAnimal.add(dog)
     listAnimal.add(fox)
     listAnimal.add(cat)
     listAnimal.forEach { println(it.eat()) }
+    listAnimal.forEach { println(it.sleep()) }
 }
