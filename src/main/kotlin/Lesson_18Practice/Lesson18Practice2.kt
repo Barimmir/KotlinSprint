af01b1cbc7c94125cafe35f10f7cabbb6317b95b
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-11-08 15:34:42 +0300

    KS-18-2-Поправил

--- src/main/kotlin/Lesson_18Practice/Lesson18Practice2.kt
+++ src/main/kotlin/Lesson_18Practice/Lesson18Practice2.kt
@@ -1,28 +1,30 @@
 package org.example.Lesson_18Practice
 
-open class Dice() {
+abstract class Dice(
+    open val facets: Int,
+) {
     open fun trowADice() = ""
 }
 
 class Dice4(
-    private val facets: Int,
-) : Dice() {
+    override val facets: Int = 4,
+) : Dice(facets) {
     override fun trowADice(): String {
         return "Выпало: ${(MIN_FACETS..facets).random()}"
     }
 }
 
 class Dice6(
-    private val facets: Int,
-) : Dice() {
+    override val facets: Int = 6,
+) : Dice(facets) {
     override fun trowADice(): String {
         return "Выпало: ${(MIN_FACETS..facets).random()}"
     }
 }
 
 class Dice8(
-    private val facets: Int
-) : Dice() {
+    override val facets: Int = 8,
+) : Dice(facets) {
     override fun trowADice(): String {
         return "Выпало: ${(MIN_FACETS..facets).random()}"
     }
@@ -30,9 +32,9 @@ class Dice8(
 
 fun main() {
     val listDice = mutableListOf<Dice>()
-    val dice4 = Dice4(4)
-    val dice6 = Dice6(6)
-    val dice8 = Dice8(8)
+    val dice4 = Dice4()
+    val dice6 = Dice6()
+    val dice8 = Dice8()
     listDice.add(dice4)
     listDice.add(dice6)
     listDice.add(dice8)
