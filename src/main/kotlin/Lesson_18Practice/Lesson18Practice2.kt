9b003a70ff68ab1254ebae1cca080e87858d2f90
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-11-08 16:59:04 +0300

    KS-18-2-Поправил 2.0

--- src/main/kotlin/Lesson_18Practice/Lesson18Practice2.kt
+++ src/main/kotlin/Lesson_18Practice/Lesson18Practice2.kt
@@ -3,42 +3,41 @@ package org.example.Lesson_18Practice
 abstract class Dice(
     open val facets: Int,
 ) {
-    open fun trowADice() = ""
+    open fun trowADice() {
+        println("Выпало ${(MIN_FACETS..facets).random()}")
+    }
 }
 
 class Dice4(
     override val facets: Int = 4,
 ) : Dice(facets) {
-    override fun trowADice(): String {
-        return "Выпало: ${(MIN_FACETS..facets).random()}"
+    override fun trowADice() {
+        super.trowADice()
     }
 }
 
 class Dice6(
     override val facets: Int = 6,
 ) : Dice(facets) {
-    override fun trowADice(): String {
-        return "Выпало: ${(MIN_FACETS..facets).random()}"
+    override fun trowADice() {
+        super.trowADice()
     }
 }
 
 class Dice8(
     override val facets: Int = 8,
 ) : Dice(facets) {
-    override fun trowADice(): String {
-        return "Выпало: ${(MIN_FACETS..facets).random()}"
+    override fun trowADice() {
+        super.trowADice()
     }
 }
 
 fun main() {
-    val listDice = mutableListOf<Dice>()
+    val listDice = listOf(Dice4(), Dice6(), Dice8())
     val dice4 = Dice4()
     val dice6 = Dice6()
     val dice8 = Dice8()
-    listDice.add(dice4)
-    listDice.add(dice6)
-    listDice.add(dice8)
-    listDice.forEach { println(it.trowADice()) }
+    listDice.forEach { it.trowADice() }
 }
 
 const val MIN_FACETS = 1
