a767909a8d8abe2ff94b4e656d6926a647b715bd
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-11-10 19:56:12 +0300

    KS-18-4-Поправил

--- src/main/kotlin/Lesson_18Practice/Lesson18Practice4.kt
+++ src/main/kotlin/Lesson_18Practice/Lesson18Practice4.kt
@@ -1,30 +1,39 @@
 package org.example.Lesson_18Practice
 
-abstract class Box() {
-    open fun getSquare() {}
+abstract class Box(
+    open val name: String,
+) {
+    open fun getSquare(): Int? = null
 }
 
-class Rectangle(
+open class Rectangle(
+    override val name: String,
     private val length: Int,
     private val width: Int,
     private val height: Int,
-) : Box() {
-    override fun getSquare() {
-        println("Площадь поверхности прямоугольника: ${FACETS_RECTANGLE * (length * width + width * height + height * length)}")
+) : Box(name) {
+    override fun getSquare(): Int {
+        val square = FACETS_RECTANGLE * (length * width + width * height + height * length)
+        return square
     }
 }
 
 class Cube(
+    override val name: String,
     private val ribLength: Int,
-) : Box() {
-    override fun getSquare() {
-        println("Площадь поверхности куба: ${FACETS_CUBE * (ribLength * ribLength)}")
+) : Rectangle(name, ribLength, ribLength, ribLength) {
+    override fun getSquare(): Int {
+        val square = FACETS_CUBE * (ribLength * ribLength)
+        return square
     }
 }
 
 fun main() {
-    val boxList = listOf(Rectangle(2, 3, 4), Cube(6))
-    boxList.forEach { it.getSquare() }
+    val boxList = listOf(
+        Rectangle("прямоугольника", 2, 3, 4),
+        Cube("куба", 6)
+    )
+    boxList.forEach { println("Площадь поверхности ${it.name}: ${it.getSquare()}") }
 }
 
 const val FACETS_RECTANGLE = 2
