a2503ce6b4f110ed072053c899eb074006a7a315
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-11-10 12:28:05 +0300

    KS-18-5-Рисование фигур

---
+++ src/main/kotlin/Lesson_18Practice/Lesson18Practice5.kt
@@ -0,0 +1,32 @@
+package org.example.Lesson_18Practice
+
+open class Screen {
+    fun drawCircle(figure: String,x: Int, y: Int){
+        println("Нарисован $figure  с координатами Х: $x || У: $y")
+    }
+    fun drawCircle(figure: String, x: Float, y: Float){
+        println("Нарисован $figure  с координатами Х: $x || У: $y")
+    }
+    fun drawSquare(figure: String, x: Int, y: Int){
+        println("Нарисован $figure  с координатами Х: $x || У: $y")
+    }
+    fun drawSquare(figure: String, x: Float, y: Float){
+        println("Нарисован $figure  с координатами Х: $x || У: $y")
+    }
+    fun drawDot(figure: String, x: Int, y: Int){
+        println("Нарисован $figure  с координатами Х: $x || У: $y")
+    }
+    fun drawDot(figure: String, x: Float, y: Float){
+        println("Нарисован $figure  с координатами Х: $x || У: $y")
+    }
+}
+
+fun main() {
+    val screen = Screen()
+    screen.drawCircle("Круг", 2, 4)
+    screen.drawCircle("Круг",4.3f, 5.5f)
+    screen.drawSquare("Квадрат", 4,9)
+    screen.drawSquare("Квадрат",2.5f, 4.7f)
+    screen.drawDot("Точка",1,1)
+    screen.drawDot("Точка", 0.5f,2.3f)
+}
