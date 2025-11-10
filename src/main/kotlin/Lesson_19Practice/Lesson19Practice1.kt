0d78fe783eddbde2d1b3f98d1de3bcf8fdd5c5ef
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-11-10 20:46:36 +0300

    KS-19-1-Поправил

--- src/main/kotlin/Lesson_19Practice/Lesson19Practice1.kt
+++ src/main/kotlin/Lesson_19Practice/Lesson19Practice1.kt
@@ -1,30 +1,15 @@
 package org.example.Lesson_19Practice
 
-enum class Fish10() {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHTING_FISH,
-}
-
-fun getFish(fish: Fish10) {
-    when (fish) {
-        Fish10.GUPPY -> println("Гуппи")
-        Fish10.ANGELFISH -> println("Скалярия")
-        Fish10.GOLDFISH -> println("Золотая рыбка")
-        Fish10.SIAMESE_FIGHTING_FISH -> println("Петушок")
-    }
+enum class Fish10(val fishNumber: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалария"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
 
 fun main() {
-    val fish = listOf(1, 2, 3, 4)
     println("Этих рыбок можно добавить в аквариум:")
-    for (i in fish) {
-        when (i) {
-            1 -> getFish(Fish10.GUPPY)
-            2 -> getFish(Fish10.ANGELFISH)
-            3 -> getFish(Fish10.GOLDFISH)
-            4 -> getFish(Fish10.SIAMESE_FIGHTING_FISH)
-        }
+    for (i in Fish10.entries) {
+        println(i.fishNumber)
     }
 }
