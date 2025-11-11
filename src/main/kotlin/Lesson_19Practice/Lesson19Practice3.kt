7d86dd2a2eaa8411032d63b65bcdc8aae61b2c47
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-11-11 11:12:40 +0300

    KS-19-3-Надо поработать, но потом

---
+++ src/main/kotlin/Lesson_19Practice/Lesson19Practice3.kt
@@ -0,0 +1,25 @@
+package org.example.Lesson_19Practice
+
+import javax.management.NotificationEmitter
+
+class Spaceship(
+    val nameShip: String,
+){
+    fun takeOff(){
+        println("Корабль $nameShip взлетает")
+    }
+    fun landing(){
+        TODO("Добавь логику,дураааашка")
+    }
+    fun shootAsteroid(){
+        //TODO Добавить логику стрельбы
+        println("Корабль $nameShip стреляет в астероиды")
+    }
+}
+
+fun main() {
+    val ship = Spaceship("Попадос")
+    ship.takeOff()
+    ship.shootAsteroid()
+    ship.landing()
+}
