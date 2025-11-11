e0c7c9c74c61dbf2b66ccfbab34c2ec9d1f0b4f8
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-11-11 10:38:17 +0300

    KS-19-2-Поправил 2.0

--- src/main/kotlin/Lesson_19Practice/Lesson19Practice1.kt
+++ src/main/kotlin/Lesson_19Practice/Lesson19Practice1.kt
@@ -1,15 +1,13 @@
 package org.example.Lesson_19Practice
 
-enum class Fish10(val fishNumber: String) {
+enum class Fish10(val fishName: String) {
     GUPPY("Гуппи"),
     ANGELFISH("Скалария"),
     GOLDFISH("Золотая рыбка"),
-    SIAMESE_FIGHTING_FISH("Петушок"),
+    SIAMESE_FIGHTING_FISH("Петушок")
 }
 
 fun main() {
     println("Этих рыбок можно добавить в аквариум:")
-    for (i in Fish10.entries) {
-        println(i.fishNumber)
-    }
+    Fish10.entries.forEach { println(it.fishName) }
 }
