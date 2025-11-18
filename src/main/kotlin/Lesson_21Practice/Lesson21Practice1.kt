b9f39c03951ae85bc01080cab927fc9773173d82
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-11-18 15:41:13 +0300

    KS-21-1-Добавил букв

--- src/main/kotlin/Lesson_21Practice/Lesson21Practice1.kt
+++ src/main/kotlin/Lesson_21Practice/Lesson21Practice1.kt
@@ -1,7 +1,7 @@
 package org.example.Lesson_21Practice
 
 fun String.vowelCount(): Int {
-    val listVowels = setOf('e', 'o')
+    val listVowels = setOf('e', 'y', 'u', 'i', 'o', 'a')
     return this.count { it in listVowels }
 }
 
