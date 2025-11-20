96ae4d3870052d25e94b8fde77df5e5bb7df62a1
GrivnoKoder <grivnoisinsaideme@gmail.com>
2025-11-20 11:56:23 +0300

    KS-22-4-MVVM

---
+++ src/main/kotlin/Lesson_22Practice/Lesson22Practice4.kt
@@ -0,0 +1,28 @@
+package org.example.Lesson_22Practice
+
+data class MainScreenState(
+    val data: String? = "Отсутствие данных",
+    val isLoading: Boolean = false,
+)
+
+class MainScreenViewModel() {
+    var mainScreenState: MainScreenState = MainScreenState()
+    fun loadData() {
+        println(
+            "Состояние: $mainScreenState"
+        )
+        mainScreenState = mainScreenState.copy(data = "Загрузка данных", isLoading = true)
+        println(
+            "Состояние: $mainScreenState"
+        )
+        mainScreenState = mainScreenState.copy(data = "Данные успешно загружены", isLoading = false)
+        println(
+            "Состояние: $mainScreenState"
+        )
+    }
+}
+
+fun main() {
+    val viewModel = MainScreenViewModel()
+    viewModel.loadData()
+}
